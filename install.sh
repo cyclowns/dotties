a57d227a6dc67686f064e4369a7eb876edcf52f6
cyclowns <notzombiedude@gmail.com>
2019-12-09 17:26:09 -0700

    add stuff i forgot about

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 # install script for hyacinth dotfiles
 # not fully fledged, only does some stuff.
+# intended for Manjaro? Manjaro Architect probabl
 
 # Install Order
 # 1. DM (probably lightdm) and X
 # 2. WM (i3-gaps)
-# 3. Various utilities needing config
+# 3. Terminal
+#    SourceCode Pro
+#    Theme? Ayu?
+#    urxvt? Alacritty?
+#    Powerline
+# 4. Various utilities needing config
 #    Spacemacs
 #    Some bar? i3-bar or lemonbar maybe
 #    Conky?
-#    Nitrogen 
+#    Nitrogen
 #    Some password lock
 
